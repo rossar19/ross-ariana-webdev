c132163f53c23719c0ca0eb7b31e80e45169f7aa
rossar19 <ari_ross19@yahoo.com>
2017-03-23 00:13:07 -0400

    cant find q

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true, limit:'50mb'}));
 app.use(bodyParser.json({limit:'50mb'}));
 
+// var passport = require('passport');
+// var cookieParser = require('cookie-parser');
+// var session = require('express-session');
+
+// app.use(session({
+// 	secret: 'this is the secret', //proc.env.session_secret
+// 	resave: true,
+// 	saveUninitialized: true
+// }));
+// app.use(cookieParser());
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.set('view engine', 'ejs');
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
